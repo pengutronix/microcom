57f8aa56abee68b5e151d306276af49a10803310
Marc Kleine-Budde <mkl@pengutronix.de>
2009-06-12 22:15:23 +0200

    add forgotten license headers to files
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- telnet.c
+++ telnet.c
@@ -1,3 +1,21 @@
+/******************************************************************
+** File: telnet.c
+** Description: the telnet part for microcom project
+**
+** Copyright (C) 2008, 2009 Sascha Hauer <s.hauer@pengutronix.de>.
+** All rights reserved.
+****************************************************************************
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details at www.gnu.org
+****************************************************************************/
+
 #include <stdlib.h>
 #include <arpa/telnet.h>
 #include <arpa/inet.h>
