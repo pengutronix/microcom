b1bd22603610199269b268d0a0fa66d3cf987bd1
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2014-01-13 09:19:56 +0100

    Add explicit copyright and license to three files
    These files were only touched by Sascha and Marc back in 2010.
    
    Acked-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>

--- parser.c
+++ parser.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2010 Sascha Hauer <s.hauer@pengutronix.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ */
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
