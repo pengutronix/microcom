pengutronix/microcom

commit 3337bd893b6ba22c5e937527fff4ca9f1df8d9d6

fix help string to include -d and -f
Also drop wrong comment about session logging

microcom.c
@@ -152,7 +152,8 @@ void main_usage(int exitcode, char *str, char *dev)
 		"    -t host:port                work in telnet (rfc2217) mode\n"
 		"    -c interface:rx_id:tx_id    work in CAN mode\n"
 		"                                default: (%s:%x:%x)\n"
-		"microcom provides session logging in microcom.log file\n",
+		"    -f                          ignore existing lock file\n"
+		"    -d                          output debugging info\n"
 		DEFAULT_DEVICE, DEFAULT_BAUDRATE,
 		DEFAULT_CAN_INTERFACE, DEFAULT_CAN_ID, DEFAULT_CAN_ID);
 	fprintf(stderr, "Exitcode %d - %s %s\n\n", exitcode, str, dev);

commit 4d2e5e4976a24d8e308c3fbb96402c384812dc39

Fix compiler warnings and segfault
Commit 3337bd893b6b dropped the comma at the end of the format string by
mistake resulting in several compiler warnings:

	microcom.c:150:3: warning: format ‘%s’ expects argument of type ‘char *’, but argument 3 has type ‘int’ [-Wformat]
	microcom.c:150:3: warning: format ‘%d’ expects argument of type ‘int’, but argument 4 has type ‘char *’ [-Wformat]
	microcom.c:150:3: warning: format ‘%s’ expects argument of type ‘char *’, but argument 5 has type ‘int’ [-Wformat]
	microcom.c:150:3: warning: format ‘%x’ expects a matching ‘unsigned int’ argument [-Wformat]

and a segfault when main_usage is called. So readd that comma.

Fixes: 3337bd893b6b (fix help string to include -d and -f)

microcom.c
@@ -145,7 +145,7 @@ void main_usage(int exitcode, char *str, char *dev)
 		"    -c interface:rx_id:tx_id    work in CAN mode\n"
 		"                                default: (%s:%x:%x)\n"
 		"    -f                          ignore existing lock file\n"
-		"    -d                          output debugging info\n"
+		"    -d                          output debugging info\n",
 		DEFAULT_DEVICE, DEFAULT_BAUDRATE,
 		DEFAULT_CAN_INTERFACE, DEFAULT_CAN_ID, DEFAULT_CAN_ID);
 	fprintf(stderr, "Exitcode %d - %s %s\n\n", exitcode, str, dev);