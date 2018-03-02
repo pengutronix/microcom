pengutronix/microcom

commit 5b32368e4b9dbea247810f135a990d37cbbed569

commands: add serial dtr/rts toggle
With this patch it's possible to enable/disable the dtr/rts output of
the serial connection. This can be used as additional trigger in several
situations.

Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
[ukl: make current_{dtr,dsr} static]
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

serial.c
@@ -21,6 +21,7 @@
 ****************************************************************************/
 
 #include <limits.h>
+#include <sys/ioctl.h>
 #include <arpa/telnet.h>
 
 #include "microcom.h"
@@ -45,6 +46,28 @@ static void init_comm(struct termios *pts)
 	 */
 	pts->c_oflag &= ~ONLCR;
 	pts->c_iflag &= ~ICRNL;
+}
+
+static int serial_set_handshake_line(struct ios_ops *ios, int pin, int enable)
+{
+	int flag;
+	int ret;
+
+	switch (pin) {
+	case PIN_DTR:
+		flag = TIOCM_DTR;
+		break;
+	case PIN_RTS:
+		flag = TIOCM_RTS;
+		break;
+	}
+
+	if (enable)
+		ret = ioctl(ios->fd, TIOCMBIC, &flag);
+	else
+		ret = ioctl(ios->fd, TIOCMBIS, &flag);
+
+	return ret;
 }
 
 static int serial_set_speed(struct ios_ops *ios, unsigned long speed)
@@ -137,6 +160,7 @@ struct ios_ops * serial_init(char *device)
 
 	ops->set_speed = serial_set_speed;
 	ops->set_flow = serial_set_flow;
+	ops->set_handshake_line = serial_set_handshake_line;
 	ops->send_break = serial_send_break;
 	ops->exit = serial_exit;
 

commands.c
@@ -72,6 +72,68 @@ static int cmd_flow(int argc, char *argv[])
 	return 0;
 }
 
+static int current_dtr = 0;
+static int current_rts = 0;
+
+static int cmd_set_handshake_line(int argc, char *argv[])
+{
+	int enable;
+	int ret = 0;
+	int pin = 0;
+
+	if (!ios->set_handshake_line) {
+		printf("function not supported \"%s\"\n", argv[0]);
+		return 1;
+	}
+
+	if (!strncmp(argv[0], "dtr", 3))
+		pin = PIN_DTR;
+	else if (!strncmp(argv[0], "rts", 3))
+		pin = PIN_RTS;
+
+	if (!pin) {
+		printf("unknown pin \"%s\"\n", argv[0]);
+		return 1;
+	}
+
+	if (argc < 2) {
+		switch (pin) {
+		case PIN_DTR:
+			printf("current dtr: \"%d\"\n", current_dtr);
+			break;
+		case PIN_RTS:
+			printf("current rts: \"%d\"\n", current_rts);
+			break;
+		}
+		return 0;
+	}
+
+	if (!strncmp(argv[1], "1", strlen(argv[1]))) {
+		enable = 1;
+	} else if (!strncmp(argv[1], "0", strlen(argv[1]))) {
+		enable = 0;
+	} else {
+		printf("unknown dtr state \"%s\"\n", argv[1]);
+		return 1;
+	}
+
+	printf("setting %s: \"%d\"\n", argv[0], enable);
+	ret = ios->set_handshake_line(ios, pin, enable);
+	if (ret)
+		return ret;
+
+	switch (pin) {
+	case PIN_DTR:
+		current_dtr = enable;
+		break;
+	case PIN_RTS:
+		current_rts = enable;
+		break;
+	}
+
+	return 0;
+}
+
 static int cmd_exit(int argc, char *argv[])
 {
 	return MICROCOM_CMD_START;
@@ -147,6 +209,16 @@ static struct cmd cmds[] = {
 		.fn = cmd_flow,
 		.info = "set flow control",
 		.help = "flow hard|soft|none",
+	}, {
+		.name = "dtr",
+		.fn = cmd_set_handshake_line,
+		.info = "set dtr value",
+		.help = "dtr 1|0",
+	}, {
+		.name = "rts",
+		.fn = cmd_set_handshake_line,
+		.info = "set rts value",
+		.help = "rts 1|0",
 	}, {
 		.name = "break",
 		.fn = cmd_break,

microcom.h
@@ -43,6 +43,9 @@ struct ios_ops {
 #define FLOW_SOFT	1
 #define FLOW_HARD	2
 	int (*set_flow)(struct ios_ops *, int flow);
+#define PIN_DTR	1
+#define PIN_RTS	2
+	int (*set_handshake_line)(struct ios_ops *, int pin, int enable);
 	int (*send_break)(struct ios_ops *);
 	void (*exit)(struct ios_ops *);
 	int fd;

commit f1e2ac47d914152e601c26460f3b37e8721495b9

serial: fix dtr/rts set and clear
The code cleared the bit with TIOCMBI_C on enable and set the bit with
TIOCMBI_S on disable (last letters highlighted for clarity). Fix the
code to use the correct command.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Acked-by: Michael Grzeschik <mgr@pengutronix.de>
Fixes: 5b32368e4b9d ("commands: add serial dtr/rts toggle")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

serial.c
@@ -63,9 +63,9 @@ static int serial_set_handshake_line(struct ios_ops *ios, int pin, int enable)
 	}
 
 	if (enable)
-		ret = ioctl(ios->fd, TIOCMBIC, &flag);
-	else
 		ret = ioctl(ios->fd, TIOCMBIS, &flag);
+	else
+		ret = ioctl(ios->fd, TIOCMBIC, &flag);
 
 	return ret;
 }