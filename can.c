pengutronix/microcom

commit dc0d7aad38f21629074416c2b2e8a5cf9c269785

can: Get rid of pthread for can transfers
Since there are ios specific read and write callbacks the needed overhead
for can transfers can be implemented in these which makes the separate
thread superfluous. This simplifies the can driver considerably.

As a consequence microcom doesn't need pthreads any more and the
respective changes in configure.ac can go away, too. The configuration
for lgtm which assert that the AX_PTHREAD macro is available can be
dropped and the travis configuration can be simplified accordingly.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Closes: https://github.com/pengutronix/microcom/issues/8

can.c
@@ -15,7 +15,6 @@
  *
  */
 
-#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string.h>
@@ -34,29 +33,59 @@
 
 #include "microcom.h"
 
-enum socket_local_enum {
-	SOCKET_CANSIDE,
-	SOCKET_TERMSIDE,
-	SOCKET_MAX,
-};
-
 struct can_data {
-	int socket_can;
-	int socket_local[SOCKET_MAX];
 	int can_id;
 };
 
 static struct can_data data;
-static pthread_t can_thread;
 
 static ssize_t can_write(struct ios_ops *ios, const void *buf, size_t count)
 {
-	return write(ios->fd, buf, count);
+	size_t loopcount;
+	ssize_t ret = 0, err;
+
+	struct can_frame to_can = {
+		.can_id = data.can_id,
+	};
+
+	while (count > 0) {
+		loopcount = min(count, sizeof(to_can.data));
+		memcpy(to_can.data, buf, loopcount);
+		to_can.can_dlc = loopcount;
+retry:
+		err = write(ios->fd, &to_can, sizeof(to_can));
+		if (err < 0 && errno == EINTR)
+			goto retry;
+
+		if (err < 0)
+			return err;
+
+		assert(err < count);
+		buf += err;
+		count -= err;
+		ret += err;
+	}
+
+	return ret;
 }
 
 static ssize_t can_read(struct ios_ops *ios, void *buf, size_t count)
 {
-	return read(ios->fd, buf, count);
+	struct can_frame from_can;
+	ssize_t ret;
+
+retry:
+	ret = read(ios->fd, &from_can, sizeof(from_can));
+	if (ret < 0 && errno != EINTR)
+		goto retry;
+
+	if (ret < 0)
+		return ret;
+
+	assert(count >= from_can.can_dlc);
+	memcpy(buf, from_can.data, from_can.can_dlc);
+
+	return from_can.can_dlc;
 }
 
 static int can_set_speed(struct ios_ops *ios, unsigned long speed)
@@ -76,73 +105,8 @@ static int can_send_break(struct ios_ops *ios)
 
 static void can_exit(struct ios_ops *ios)
 {
-	shutdown(ios->fd, SHUT_RDWR);
 	close(ios->fd);
-	pthread_join(can_thread, NULL);
 	free(ios);
-}
-
-static void *can_thread_fun(void *_data)
-{
-	struct can_data *data = _data;
-	struct can_frame to_can = {
-		.can_id = data->can_id,
-	};
-	struct can_frame from_can;
-	fd_set ready;
-	int ret, fd_max;
-
-	fd_max = max(data->socket_can, data->socket_local[SOCKET_CANSIDE]);
-	fd_max++;
-
-	while (1) {
-		FD_ZERO(&ready);
-		FD_SET(data->socket_can, &ready);
-		FD_SET(data->socket_local[SOCKET_CANSIDE], &ready);
-
-		ret = select(fd_max, &ready, NULL, NULL, NULL);
-		if (ret == -1) {
-			if (errno == EINTR)
-				continue;
-			goto exit;
-		}
-
-		/* CAN -> TERMINAL */
-		if (FD_ISSET(data->socket_can, &ready)) {
-			ret = read(data->socket_can, &from_can, sizeof(from_can));
-			if ((ret == -1 && errno != EINTR) ||
-			    ret != sizeof(from_can))
-				goto exit;
-
-			ret = write(data->socket_local[SOCKET_CANSIDE],
-				    from_can.data, from_can.can_dlc);
-			if ((ret == -1 && errno != EINTR) ||
-			    ret != from_can.can_dlc)
-				goto exit;
-		}
-
-		/* TERMINAL -> CAN */
-		if (FD_ISSET(data->socket_local[SOCKET_CANSIDE], &ready)) {
-			ret = read(data->socket_local[SOCKET_CANSIDE],
-				   to_can.data, sizeof(to_can.data));
-			if ((ret == -1 && errno != EINTR) ||
-			    ret == 0)
-				goto exit;
-
-			to_can.can_dlc = ret;
-			ret = write(data->socket_can, &to_can, sizeof(to_can));
-			if ((ret == -1 && errno != EINTR) ||
-			    ret != sizeof(to_can))
-				goto exit;
-		}
-	}
-
- exit:
-	shutdown(data->socket_local[SOCKET_CANSIDE], SHUT_RDWR);
-	close(data->socket_local[SOCKET_CANSIDE]);
-	close(data->socket_can);
-
-	return NULL;
 }
 
 struct ios_ops *can_init(char *interface_id)
@@ -201,39 +165,30 @@ struct ios_ops *can_init(char *interface_id)
 
 	/* no cleanups on failure, we exit anyway */
 
-	data.socket_can = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-	if (data.socket_can < 0) {
+	ios->fd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (ios->fd < 0) {
 		perror("socket");
 		return NULL;
 	}
 
-	if (setsockopt(data.socket_can, SOL_CAN_RAW, CAN_RAW_FILTER,
+	if (setsockopt(ios->fd, SOL_CAN_RAW, CAN_RAW_FILTER,
 		       filter, sizeof(filter))) {
 		perror("setsockopt");
 		return NULL;
 	}
 
 	strcpy(ifr.ifr_name, interface);
-	if (ioctl(data.socket_can, SIOCGIFINDEX, &ifr)) {
+	if (ioctl(ios->fd, SIOCGIFINDEX, &ifr)) {
 		printf("%s: %s\n", interface, strerror(errno));
 		return NULL;
 	}
 	addr.can_ifindex = ifr.ifr_ifindex;
 
-	if (bind(data.socket_can, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+	if (bind(ios->fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 		return NULL;
 	}
 
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, data.socket_local) < 0) {
-		perror("socketpair");
-		return NULL;
-	}
-
-	if (pthread_create(&can_thread, NULL, can_thread_fun, &data) != 0)
-		return NULL;
-
-	ios->fd = data.socket_local[SOCKET_TERMSIDE];
 	printf("connected to %s (rx_id=%x, tx_id=%x)\n",
 	       interface, filter->can_id, data.can_id);
 

.travis.yml
@@ -1,10 +1,6 @@
 language: c
 compiler: gcc
 dist: trusty
-addons:
-  apt:
-    packages:
-      - autoconf-archive
 script:
   - autoreconf -fi
   - ./configure

configure.ac
@@ -9,7 +9,6 @@ AC_CONFIG_HEADERS([config.h])
 
 # Checks for programs.
 AC_PROG_CC
-AX_PTHREAD
 AC_SYS_LARGEFILE
 
 # Checks for libraries.
@@ -38,10 +37,7 @@ AS_IF([test "x$enable_can" != "xno"],
       ])
 
 AS_IF([test "x$enable_can" = "xyes"],
-      [AC_DEFINE([USE_CAN], [1], [Define if can mode should be built-in])
-       LIBS="$PTHREAD_LIBS $LIBS"
-       CFLAGS="$CFLAGS $PTHREAD_CFLAGS"
-       CC="$PTHREAD_CC"])
+      [AC_DEFINE([USE_CAN], [1], [Define if can mode should be built-in])])
 
 AM_CONDITIONAL([CAN], [test "x$enable_can" = "xyes"])
 

lgtm.yml
@@ -1,5 +1,0 @@
-extraction:
-    cpp:
-        prepare:
-            packages:
-                - autoconf-archive

commit d536e6e0816afb260913909aad11fa86757465a2

can: fix failing assertion and counter in CAN mode
This patch fixes the following assertion:

    microcom: can.c:63: can_write: Assertion `err < count' failed.

    #0  __GI_raise (sig=sig@entry=6) at ../sysdeps/unix/sysv/linux/raise.c:50
    #1  0x00007ffff7db6535 in __GI_abort () at abort.c:79
    #2  0x00007ffff7db640f in __assert_fail_base (fmt=0x7ffff7f18ee0 "%s%s%s:%u: %s%sAssertion `%s' failed.\n%n", assertion=0x55555555c3f3 "err < count", file=0x55555555c3d3 "can.c", line=63, function=<optimized out>) at assert.c:92
    #3  0x00007ffff7dc4102 in __GI___assert_fail (assertion=assertion@entry=0x55555555c3f3 "err < count", file=file@entry=0x55555555c3d3 "can.c", line=line@entry=63, function=function@entry=0x55555555c440 <__PRETTY_FUNCTION__.4646> "can_write") at assert.c:101
    #4  0x0000555555559cff in can_write (ios=0x555555560260, buf=0x7fffffffdb30, count=1) at can.c:63
    #5  can_write (ios=0x555555560260, buf=0x7fffffffdb30, count=1) at can.c:42
    #6  0x00005555555584d9 in cook_buf (num=1, buf=0x7fffffffdb30 "t", ios=<optimized out>) at mux.c:390
    #7  mux_loop (ios=0x555555560260) at mux.c:479
    #8  0x00005555555565e2 in main (argc=3, argv=0x7fffffffe2c8) at microcom.c:330

The write call returns the size of the whole CAN frame, which is
sizeof(struct can_frame), regardless of the length of the data. Use this
in the assertion.

For the same reason, we have to use struct can_frame::can_dlc as the
couter of sent bytes. This patch also fixes that problem.

Fixes: dc0d7aad38f2 ("can: Get rid of pthread for can transfers")
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

can.c
@@ -60,10 +60,10 @@ retry:
 		if (err < 0)
 			return err;
 
-		assert(err < count);
-		buf += err;
-		count -= err;
-		ret += err;
+		assert(err == sizeof(to_can));
+		buf += loopcount;
+		count -= loopcount;
+		ret += loopcount;
 	}
 
 	return ret;