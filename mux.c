pengutronix/microcom

commit 1334a93b2718132eaa5689afdb9c2aaae0db3eaa

implement telnet rfc2217 mode (experimental), update help
git-svn-id: https://svn.pengutronix.de/svn/microcom/trunk@6 b6a597e7-0b13-0410-876a-e2d53f4c184b

mux.c
@@ -18,6 +18,7 @@
 ** Rev. 1.0 - Feb. 2000
 ****************************************************************************/
 #include "microcom.h"
+#include <arpa/telnet.h>
 
 #define SCRIPT_DELAY 1
 #define BUFSIZE 1024
@@ -25,14 +26,172 @@
 extern int dolog;
 extern FILE *flog;
 
+int do_com_port_option(unsigned char *buf, int len)
+{
+	int i = 0;
+
+	while (i < len) {
+		switch (buf[i]) {
+		case IAC:
+			dprintf("IAC ");
+			return i + 1;
+		case SET_BAUDRATE_CS:
+			dprintf("SET_BAUDRATE_CS ");
+			break;
+		case SET_DATASIZE_CS:
+			dprintf("SET_DATASIZE_CS ");
+			break;
+		case SET_PARITY_CS:
+			dprintf("SET_PARITY_CS ");
+			break;
+		case SET_STOPSIZE_CS:
+			dprintf("SET_STOPSIZE_CS ");
+			break;
+		case SET_CONTROL_CS:
+			dprintf("SET_CONTROL_CS ");
+			break;
+		case NOTIFY_LINESTATE_CS:
+			dprintf("NOTIFY_LINESTATE_CS ");
+			break;
+		case NOTIFY_MODEMSTATE_CS:
+			dprintf("NOTIFY_MODEMSTATE_CS ");
+			break;
+		case FLOWCONTROL_SUSPEND_CS:
+			dprintf("FLOWCONTROL_SUSPEND_CS ");
+			break;
+		case FLOWCONTROL_RESUME_CS:
+			dprintf("FLOWCONTROL_RESUME_CS ");
+			break;
+		case SET_LINESTATE_MASK_CS:
+			dprintf("SET_LINESTATE_MASK_CS ");
+			break;
+		case SET_MODEMSTATE_MASK_CS:
+			dprintf("SET_MODEMSTATE_MASK_CS ");
+			break;
+		case PURGE_DATA_CS:
+			dprintf("PURGE_DATA_CS ");
+			break;
+		case SET_BAUDRATE_SC:
+			dprintf("SET_BAUDRATE_SC %d ", ntohl(*(int *)&buf[i + 1]));
+			i += 4;
+			break;
+		case SET_DATASIZE_SC:
+			dprintf("SET_DATASIZE_SC ");
+			break;
+		case SET_PARITY_SC:
+			dprintf("SET_PARITY_SC ");
+			break;
+		case SET_STOPSIZE_SC:
+			dprintf("SET_STOPSIZE_SC ");
+			break;
+		case SET_CONTROL_SC:
+			i++;
+			dprintf("SET_CONTROL_SC 0x%02x ", buf[i]);
+			break;
+		case NOTIFY_LINESTATE_SC:
+			dprintf("NOTIFY_LINESTATE_SC ");
+			break;
+		case NOTIFY_MODEMSTATE_SC:
+			i++;
+			dprintf("NOTIFY_MODEMSTATE_SC 0x%02x ", buf[i]);
+			break;
+		case FLOWCONTROL_SUSPEND_SC:
+			dprintf("FLOWCONTROL_SUSPEND_SC ");
+			break;
+		case FLOWCONTROL_RESUME_SC:
+			dprintf("FLOWCONTROL_RESUME_SC ");
+			break;
+		case SET_LINESTATE_MASK_SC:
+			dprintf("SET_LINESTATE_MASK_SC ");
+			break;
+		case SET_MODEMSTATE_MASK_SC:
+			dprintf("SET_MODEMSTATE_MASK_SC ");
+			break;
+		case PURGE_DATA_SC:
+			dprintf("PURGE_DATA_SC ");
+			break;
+		default:
+			dprintf("%d ", buf[i]);
+			break;
+		}
+		i++;
+	}
+
+	return len;
+}
+
+int do_subneg(unsigned char *buf, int len)
+{
+	int i = 0;
+
+	while (i < len) {
+		switch (buf[i]) {
+		case COM_PORT_OPTION:
+			dprintf("COM_PORT_OPTION ");
+			return do_com_port_option(&buf[i + 1], len - i) + 1;
+		case IAC:
+			dprintf("IAC ");
+			return len - i;
+		default:
+			dprintf("%d ", buf[i]);
+			break;
+		}
+		i++;
+	}
+
+	return len;
+}
+
+int handle_command(unsigned char *buf, int len)
+{
+	int i = 0, a;
+
+	while (i < len) {
+		switch (buf[i]) {
+		case SB:
+			dprintf("SB ");
+			i += do_subneg(&buf[i+1], len - i);
+			break;
+		case IAC:
+			dprintf("IAC ");
+			break;
+		case COM_PORT_OPTION:
+			dprintf("COM_PORT_OPTION ");
+			break;
+		case SE:
+			dprintf("SE ");
+			break;
+		case WILL:
+			dprintf("WILL ");
+			break;
+		case WONT:
+			dprintf("WILL ");
+			break;
+		case DO:
+			dprintf("WILL ");
+			break;
+		case DONT:
+			dprintf("WILL ");
+			break;
+		default:
+			dprintf("%d ", buf[i]);
+			break;
+		}
+		i++;
+	}
+
+	dprintf("\n");
+	return len;
+}
+
 /* main program loop */
 void
 mux_loop(int pf)
 {
 	fd_set ready;		/* used for select */
-	int i = 0;		/* used in the multiplex loop */
+	int i = 0, len;		/* used in the multiplex loop */
 	int done = 0;
-	char buf[BUFSIZE];
+	unsigned char buf[BUFSIZE];
 	struct timeval tv;
 
 	tv.tv_sec = SCRIPT_DELAY;
@@ -46,10 +205,13 @@ mux_loop(int pf)
 		select(pf + 1, &ready, NULL, NULL, NULL);
 
 		if (FD_ISSET(pf, &ready)) {
+			i = 0;
 			/* pf has characters for us */
-			i = read(pf, buf, BUFSIZE);
-			if (i > 0) {
-				write(STDOUT_FILENO, buf, i);
+			len = read(pf, buf, BUFSIZE);
+			if (len > 0) {
+				if (*buf == IAC)
+					i = handle_command(buf, len);
+				write(STDOUT_FILENO, buf + i, len - i);
 				if (dolog) {
 					fwrite(buf, 1, i, flog);
 					fflush(flog);

help.c
@@ -20,11 +20,13 @@
 #include "microcom.h"
 
 #include <sys/ioctl.h>
+#include <arpa/telnet.h>
 
 extern int crnl_mapping;	//0 - no mapping, 1 mapping
 extern char device[MAX_DEVICE_NAME];	/* serial device name */
 extern int dolog;			/* log active flag */
 extern FILE *flog;		/* log file */
+extern int telnet;
 
 static int help_state = 0;
 static int in_escape = 0;
@@ -225,26 +227,20 @@ help_set_terminal(int fd, char c)
 	case 'h':		/* hardware flow control */
 		in_escape = 0;	/* get it out from escape state */
 		help_state = 0;
+		ios->set_flow(fd, FLOW_HARD);
 		/* hardware flow control */
-		pts.c_cflag |= CRTSCTS;
-		pts.c_iflag &= ~(IXON | IXOFF | IXANY);
-		tcsetattr(fd, TCSANOW, &pts);
 		break;
 	case 's':		/* software flow contrlol */
 		in_escape = 0;	/* get it out from escape state */
 		help_state = 0;
+		ios->set_flow(fd, FLOW_SOFT);
 		/* software flow control */
-		pts.c_cflag &= ~CRTSCTS;
-		pts.c_iflag |= IXON | IXOFF | IXANY;
-		tcsetattr(fd, TCSANOW, &pts);
 		break;
 	case 'n':		/* no flow control */
 		in_escape = 0;	/* get it out from escape state */
 		help_state = 0;
 		/* no flow control */
-		pts.c_cflag &= ~CRTSCTS;
-		pts.c_iflag &= ~(IXON | IXOFF | IXANY);
-		tcsetattr(fd, TCSANOW, &pts);
+		ios->set_flow(fd, FLOW_NONE);
 		break;
 	case '~':
 	case 'q':
@@ -282,6 +278,7 @@ help_set_speed(int fd, char c)
 		B460800
 	};
 
+
 	if (c < 'a' && c > 'j') {
 		if (c == '~') {
 			help_speed();
@@ -294,10 +291,8 @@ help_set_speed(int fd, char c)
 		return;
 	}
 
-	tcgetattr(fd, &pts);
-	cfsetospeed(&pts, speed[c - 'a']);
-	cfsetispeed(&pts, speed[c - 'a']);
-	tcsetattr(fd, TCSANOW, &pts);
+	ios->set_speed(fd, speed[c - 'a']);
+
 	in_escape = 0;
 	help_state = 0;
 	help_done();

microcom.c
@@ -23,17 +23,22 @@
 
 #include <unistd.h>
 #include <getopt.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <arpa/telnet.h>
 
 int crnl_mapping;		//0 - no mapping, 1 mapping
-char device[MAX_DEVICE_NAME];	/* serial device name */
 int dolog = 0;			/* log active flag */
 FILE *flog;			/* log file */
 int pf = 0;			/* port file descriptor */
 struct termios pots;		/* old port termios settings to restore */
 struct termios sots;		/* old stdout/in termios settings to restore */
 
-void
-init_comm(struct termios *pts, int speed)
+int telnet = 0;
+struct ios_ops *ios;
+int debug = 0;
+
+static void init_comm(struct termios *pts)
 {
 	/* some things we want to set arbitrarily */
 	pts->c_lflag &= ~ICANON;
@@ -51,17 +56,9 @@ init_comm(struct termios *pts, int speed)
 	pts->c_oflag &= ~ONLCR;
 	crnl_mapping = 0;
 	pts->c_iflag &= ~ICRNL;
-
-	/* set hardware flow control by default */
-	pts->c_cflag &= ~CRTSCTS;
-	pts->c_iflag &= ~(IXON | IXOFF | IXANY);
-
-	cfsetospeed(pts, speed);
-	cfsetispeed(pts, speed);
 }
 
-void
-init_stdin(struct termios *sts)
+void init_stdin(struct termios *sts)
 {
 	/* again, some arbitrary things */
 	sts->c_iflag &= ~BRKINT;
@@ -73,6 +70,144 @@ init_stdin(struct termios *sts)
 	/* no local echo: allow the other end to do the echoing */
 	sts->c_lflag &= ~(ECHO | ECHOCTL | ECHONL);
 }
+
+speed_t baudrate_to_flag(int speed)
+{
+	switch(speed) {
+	case 50: return B50;
+	case 75: return B75;
+	case 110: return B110;
+	case 134: return B134;
+	case 150: return B150;
+	case 200: return B200;
+	case 300: return B300;
+	case 600: return B600;
+	case 1200: return B1200;
+	case 1800: return B1800;
+	case 2400: return B2400;
+	case 4800: return B4800;
+	case 9600: return B9600;
+	case 19200: return B19200;
+	case 38400: return B38400;
+	case 57600: return B57600;
+	case 115200: return B115200;
+	case 230400: return B230400;
+	default:
+		printf("unknown speed: %d\n",speed);
+		return -1;
+	}
+}
+
+int flag_to_baudrate(speed_t speed)
+{
+	switch(speed) {
+	case B50: return 50;
+	case B75: return 75;
+	case B110: return 110;
+	case B134: return 134;
+	case B150: return 150;
+	case B200: return 200;
+	case B300: return 300;
+	case B600: return 600;
+	case B1200: return 1200;
+	case B1800: return 1800;
+	case B2400: return 2400;
+	case B4800: return 4800;
+	case B9600: return 9600;
+	case B19200: return 19200;
+	case B38400: return 38400;
+	case B57600: return 57600;
+	case B115200: return 115200;
+	case B230400: return 230400;
+	default:
+		printf("unknown speed: %d\n",speed);
+		return -1;
+	}
+}
+
+int serial_set_speed(int fd, speed_t speed)
+{
+	struct termios pts;	/* termios settings on port */
+
+	tcgetattr(fd, &pts);
+	cfsetospeed(&pts, speed);
+	cfsetispeed(&pts, speed);
+	tcsetattr(fd, TCSANOW, &pts);
+
+	return 0;
+}
+
+int serial_set_flow(int fd, int flow)
+{
+	struct termios pts;	/* termios settings on port */
+	tcgetattr(fd, &pts);
+
+	switch (flow) {
+	case FLOW_NONE:
+		/* no flow control */
+		pts.c_cflag &= ~CRTSCTS;
+		pts.c_iflag &= ~(IXON | IXOFF | IXANY);
+		break;
+	case FLOW_HARD:
+		/* hardware flow control */
+		pts.c_cflag |= CRTSCTS;
+		pts.c_iflag &= ~(IXON | IXOFF | IXANY);
+		break;
+	case FLOW_SOFT:
+		/* software flow control */
+		pts.c_cflag &= ~CRTSCTS;
+		pts.c_iflag |= IXON | IXOFF | IXANY;
+		break;
+	}
+
+	tcsetattr(fd, TCSANOW, &pts);
+
+	return 0;
+}
+
+struct ios_ops serial_ops = {
+	.set_speed = serial_set_speed,
+	.set_flow = serial_set_flow,
+};
+
+int telnet_set_speed(int fd, speed_t speed)
+{
+
+//	unsigned char buf1[] = {IAC, WILL , COM_PORT_OPTION};
+	unsigned char buf2[] = {IAC, SB, COM_PORT_OPTION, SET_BAUDRATE_CS, 0, 0, 0, 0, IAC, SE};
+	int *speedp = (int *)&buf2[4];
+
+//	write(fd, buf1, 3);
+	*speedp = htonl(flag_to_baudrate(speed));
+	write(fd, buf2, 10);
+}
+
+int telnet_set_flow(int fd, int flow)
+{
+	unsigned char buf2[] = {IAC, SB, COM_PORT_OPTION, SET_CONTROL_CS, 0, IAC, SE};
+
+	switch (flow) {
+	case FLOW_NONE:
+		/* no flow control */
+		buf2[4] = 1;
+		break;
+	case FLOW_SOFT:
+		/* software flow control */
+		buf2[4] = 2;
+		break;
+	case FLOW_HARD:
+		/* hardware flow control */
+		buf2[4] = 3;
+		break;
+	}
+	write(fd, buf2, sizeof(buf2));
+
+}
+
+struct ios_ops telnet_ops = {
+	.set_speed = telnet_set_speed,
+	.set_flow = telnet_set_flow,
+};
 
 /********************************************************************
  Main functions
@@ -87,23 +222,21 @@ init_stdin(struct termios *sts)
  int main(int argc, char *argv[]) -
       main program function
 ********************************************************************/
-void
-main_usage(int exitcode, char *str, char *dev)
+void main_usage(int exitcode, char *str, char *dev)
 {
 	fprintf(stderr, "Usage: microcom [options]\n"
 		" [options] include:\n"
-		"    -pdevfile       use the specified serial port device;\n"
-		"                    if a port is not provided, microcom\n"
-		"                        will try to autodetect a modem\n"
-		"           example: -p/dev/ttyS3\n"
-		"microcom provides session logging in microcom.log file\n");
+		"    -p devfile      use the specified serial port device (%s);\n"
+		"    -s speed        use specified baudrate (%d)\n"
+		"    -t host:port    work in telnet (rfc2217) mode\n"
+		"microcom provides session logging in microcom.log file\n",
+		DEFAULT_DEVICE, DEFAULT_BAUDRATE);
 	fprintf(stderr, "Exitcode %d - %s %s\n\n", exitcode, str, dev);
 	exit(exitcode);
 }
 
 /* restore original terminal settings on exit */
-void
-cleanup_termios(int signal)
+void cleanup_termios(int signal)
 {
 	/* cloase the log file first */
 	if (dolog) {
@@ -115,25 +248,25 @@ cleanup_termios(int signal)
 	exit(0);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	struct termios pts;	/* termios settings on port */
 	struct termios sts;	/* termios settings on stdout/in */
 	struct sigaction sact;	/* used to initialize the signal handler */
-	int opt,i, speed;
-	int bspeed = B115200;
-
-	device[0] = '\0';
+	int opt,i, speed = DEFAULT_BAUDRATE;
+	char *hostport;
+	char *device = DEFAULT_DEVICE;
 
 	struct option long_options[] = {
 		{ "help", no_argument, 0, 'h' },
 		{ "port", required_argument, 0, 'p'},
 		{ "speed", required_argument, 0, 's'},
+		{ "telnet", required_argument, 0, 't'},
+		{ "debug", no_argument, 0, 'd' },
 		{ 0, 0, 0, 0},
 	};
 
-	while ((opt = getopt_long(argc, argv, "hp:s:", long_options, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "hp:s:t:d", long_options, NULL)) != -1) {
 		switch (opt) {
 			case 'h':
 				main_usage(1, "", "");
@@ -143,44 +276,74 @@ main(int argc, char *argv[])
 				break;
 			case 's':
 				speed = strtoul(optarg, NULL, 0);
-				printf("speed: %d\n",speed);
-				switch(speed) {
-					case 50: bspeed = B50; break;
-					case 75: bspeed = B75; break;
-					case 110: bspeed = B110; break;
-					case 134: bspeed = B134; break;
-					case 150: bspeed = B150; break;
-					case 200: bspeed = B200; break;
-					case 300: bspeed = B300; break;
-					case 600: bspeed = B600; break;
-					case 1200: bspeed = B1200; break;
-					case 1800: bspeed = B1800; break;
-					case 2400: bspeed = B2400; break;
-					case 4800: bspeed = B4800; break;
-					case 9600: bspeed = B9600; break;
-					case 19200: bspeed = B19200; break;
-					case 38400: bspeed = B38400; break;
-					case 57600: bspeed = B57600; break;
-					case 115200: bspeed = B115200; break;
-					case 230400: bspeed = B230400; break;
-					default:
-						printf("unknown speed: %d\n",speed);
-						exit(1);
-				}
 				break;
+			case 't':
+				telnet = 1;
+				hostport = optarg;
+				break;
+			case 'd':
+				debug = 1;
 		}
 	}
 
-	/* open the device */
-	pf = open(device, O_RDWR);
-	if (pf < 0)
-		main_usage(2, "cannot open device", device);
+	printf("*** Welcome to microcom ***\n");
 
-	/* modify the port configuration */
-	tcgetattr(pf, &pts);
-	memcpy(&pots, &pts, sizeof (pots));
-	init_comm(&pts, bspeed);
-	tcsetattr(pf, TCSANOW, &pts);
+	if (telnet) {
+		int sock;
+		struct sockaddr_in server_in;
+		char *host = hostport;
+		char *portstr;
+		int port;
+
+		ios = &telnet_ops;
+
+		portstr = strchr(hostport, ':');
+		if (portstr) {
+			*portstr = 0;
+			portstr++;
+			port = atoi(portstr);
+		}
+
+		memset(&server_in, 0, sizeof(server_in));     /* Zero out structure */
+		server_in.sin_family      = AF_INET;             /* Internet address family */
+		server_in.sin_addr.s_addr = inet_addr(host);   /* Server IP address */
+		server_in.sin_port        = htons(port); /* Server port */
+
+		sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (sock < 0) {
+			printf("socket() failed\n");
+			exit(1);
+		}
+
+		/* Establish the connection to the echo server */
+		if (connect(sock, (struct sockaddr *) &server_in, sizeof(server_in)) < 0) {
+			printf("connect() failed\n");
+			exit(1);
+		}
+		pf = sock;
+		printf("connected to %s (port %d)\n", host, port);
+	} else {
+		ios = &serial_ops;
+
+		/* open the device */
+		pf = open(device, O_RDWR);
+		if (pf < 0)
+			main_usage(2, "cannot open device", device);
+
+		/* modify the port configuration */
+		tcgetattr(pf, &pts);
+		memcpy(&pots, &pts, sizeof (pots));
+		init_comm(&pts);
+		tcsetattr(pf, TCSANOW, &pts);
+		printf("connected to %s\n", device);
+	}
+
+	speed = baudrate_to_flag(speed);
+		if (speed < 0)
+			exit(1);
+
+	ios->set_speed(pf, speed);
+	ios->set_flow(pf, FLOW_NONE);
 
 	/* Now deal with the local terminal side */
 	tcgetattr(STDIN_FILENO, &sts);
@@ -199,9 +362,11 @@ main(int argc, char *argv[])
 	/* run thhe main program loop */
 	mux_loop(pf);
 
-	/* restore original terminal settings and exit */
-	tcsetattr(pf, TCSANOW, &pots);
-	tcsetattr(STDIN_FILENO, TCSANOW, &sots);
+	if (!telnet) {
+		/* restore original terminal settings and exit */
+		tcsetattr(pf, TCSANOW, &pots);
+		tcsetattr(STDIN_FILENO, TCSANOW, &sots);
+	}
 
 	exit(0);
 

microcom.h
@@ -35,6 +35,9 @@
 #define MAX_SCRIPT_NAME 20 /* maximum length of the name of the script file */
 #define MAX_DEVICE_NAME 20 /* maximum length of the name of the /dev comm port driver */
 
+#define DEFAULT_BAUDRATE 115200
+#define DEFAULT_DEVICE "/dev/ttyS0"
+
 void cook_buf(int fd, char *buf, int num); /* microcom.c */ 
 void mux_loop(int pf); /* mux.c */
 
@@ -45,14 +48,53 @@ typedef enum {
   S_MAX			/* not used - just for checking */
 } S_ORIGINATOR;
 
+struct ios_ops {
+	int (*set_speed)(int fd, speed_t speed);
+#define FLOW_NONE	0
+#define FLOW_SOFT	1
+#define FLOW_HARD	2
+	int (*set_flow)(int fd, int flow);
+};
+
 int script_process(S_ORIGINATOR orig, char* buf, int size); /* script.c */
 void script_init(char* s); /* script.c */
 void mux_clear_sflag(void); /* mus.c */
 void cleanup_termios(int signal);
 void init_stdin(struct termios *sts);
-void init_comm(struct termios *pts, int speed);
 void main_usage(int exitcode, char *str, char *dev);
-   
+
+extern struct ios_ops *ios;
+extern int debug;
+
+#define dprintf(fmt,args...)  ({ if (debug) printf (fmt ,##args); })
+
+/* RFC2217 */
+#define COM_PORT_OPTION		 44
+#define SET_BAUDRATE_CS		  1
+#define SET_DATASIZE_CS		  2
+#define SET_PARITY_CS		  3
+#define SET_STOPSIZE_CS		  4
+#define SET_CONTROL_CS		  5
+#define NOTIFY_LINESTATE_CS	  6
+#define NOTIFY_MODEMSTATE_CS	  7
+#define FLOWCONTROL_SUSPEND_CS	  8
+#define FLOWCONTROL_RESUME_CS	  9
+#define SET_LINESTATE_MASK_CS	 10
+#define SET_MODEMSTATE_MASK_CS	 11
+#define PURGE_DATA_CS		 12
+#define SET_BAUDRATE_SC		101
+#define SET_DATASIZE_SC		102
+#define SET_PARITY_SC		103
+#define SET_STOPSIZE_SC		104
+#define SET_CONTROL_SC		105
+#define NOTIFY_LINESTATE_SC	106
+#define NOTIFY_MODEMSTATE_SC	107
+#define FLOWCONTROL_SUSPEND_SC	108
+#define FLOWCONTROL_RESUME_SC	109
+#define SET_LINESTATE_MASK_SC	110
+#define SET_MODEMSTATE_MASK_SC	111
+#define PURGE_DATA_SC		112
+
 #endif /* MICROCOM_H */
 
 

commit 91182380f2234ad629238bf7d323b8087ba9fddf

mux: Handle parsing subnegotiations including IACs
The IACs in a subnegotiation are quoted, too. So unquote while parsing.

If the remote rfc2217 side sends (raw)

	SB COM_PORT_CONTROL NOTIFY_MODEMSTATE_SC 0xff 0xff IAC SE

the double 0xff is to be interpreted as a single 0xff (as IAC = 0xff).

Instead the second 0xff was interpreted as quoting the following IAC and
so the end of the subnegotiation is missed making microcom exit with

	Incomplete SB string

Reported-by: James Puderer <jhpuderer@gmail.com>
Fixes: 1334a93b2718 ("implement telnet rfc2217 mode (experimental), update help")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

mux.c
@@ -27,6 +27,48 @@
 
 #define BUFSIZE 1024
 
+static ssize_t get(unsigned char *buf, unsigned char *out, size_t len)
+{
+	if (!len)
+		return -1;
+
+	if (buf[0] == IAC) {
+		if (len < 1)
+			return -1;
+		if (buf[1] == IAC) {
+			*out = IAC;
+			return 2;
+		}
+		return -1;
+	} else {
+		*out = buf[0];
+		return 1;
+	}
+}
+
+static size_t getl(unsigned char *buf, uint32_t *out, size_t len)
+{
+	*out = 0;
+	int i;
+	size_t offset = 0;
+
+	for (i = 0; i < 4; ++i) {
+		ssize_t getres;
+		unsigned char c;
+
+		getres = get(buf + offset, &c, len - offset);
+		if (getres < 0)
+			return getres;
+
+		*out <<= 8;
+		*out |= c;
+
+		offset += getres;
+	}
+
+	return offset;
+}
+
 /* This is called with buf[-2:0] being IAC SB COM_PORT_OPTION */
 static int do_com_port_option(struct ios_ops *ios, unsigned char *buf, int len)
 {
@@ -70,9 +112,17 @@ static int do_com_port_option(struct ios_ops *ios, unsigned char *buf, int len)
 		dbg_printf("PURGE_DATA_CS ");
 		break;
 	case SET_BAUDRATE_SC:
-		dbg_printf("SET_BAUDRATE_SC %d ",
-			buf[2] << 24 | buf[3] << 16 | buf[4] << 8 | buf[5]);
-		i += 4;
+		{
+			uint32_t baudrate;
+			ssize_t getres = getl(buf + 2, &baudrate, len - 2);
+
+			if (getres < 0) {
+				fprintf(stderr, "Incomplete or broken SB (SET_BAUDRATE_SC)\n");
+				return getres;
+			}
+			dbg_printf("SET_BAUDRATE_SC %u ", baudrate);
+			i += getres;;
+		}
 		break;
 	case SET_DATASIZE_SC:
 		dbg_printf("SET_DATASIZE_SC ");
@@ -84,15 +134,35 @@ static int do_com_port_option(struct ios_ops *ios, unsigned char *buf, int len)
 		dbg_printf("SET_STOPSIZE_SC ");
 		break;
 	case SET_CONTROL_SC:
-		i++;
-		dbg_printf("SET_CONTROL_SC 0x%02x ", buf[2]);
+		{
+			unsigned char ctrl;
+			ssize_t getres = get(buf + 2, &ctrl, len - 2);
+
+			if (getres < 0) {
+				fprintf(stderr, "Incomplete or broken SB (SET_CONTROL_SC)\n");
+				return getres;
+			}
+
+			dbg_printf("SET_CONTROL_SC 0x%02x ", ctrl);
+			i += getres;
+		}
 		break;
 	case NOTIFY_LINESTATE_SC:
 		dbg_printf("NOTIFY_LINESTATE_SC ");
 		break;
 	case NOTIFY_MODEMSTATE_SC:
-		i++;
-		dbg_printf("NOTIFY_MODEMSTATE_SC 0x%02x ", buf[2]);
+		{
+			unsigned char ms;
+			ssize_t getres = get(buf + 2, &ms, len - 2);
+
+			if (getres < 0) {
+				fprintf(stderr, "Incomplete or broken SB (NOTIFY_MODEMSTATE_SC)\n");
+				return getres;
+			}
+
+			dbg_printf("NOTIFY_MODEMSTATE_SC 0x%02x ", ms);
+			i += getres;
+		}
 		break;
 	case FLOWCONTROL_SUSPEND_SC:
 		dbg_printf("FLOWCONTROL_SUSPEND_SC ");