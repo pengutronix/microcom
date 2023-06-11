pengutronix/microcom

commit 48f4fd57802b5748235e18dc1ba367e675cd1109

telnet: Quote IAC on sending
To send a single IAC in the payload, you have to send two IACs, the
first quoting the second. Otherwise the sequence will be interpreted as
a command by the other side.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

telnet.c
@@ -29,7 +29,33 @@
 
 static ssize_t telnet_write(struct ios_ops *ios, const void *buf, size_t count)
 {
-	return write(ios->fd, buf, count);
+	size_t handled = 0;
+	ssize_t ret;
+	void *iac;
+
+	/*
+	 * To send an IAC character in the data stream, two IACs must be sent.
+	 * So find the first IAC in the data to be send (if any), send the data
+	 * before that IAC unquoted, then send the double IAC. Repeat until
+	 * all IACs are handled.
+	 */
+	while ((iac = memchr(buf + handled, IAC, count - handled)) != NULL) {
+		if (iac - (buf + handled)) {
+			ret = write(ios->fd, buf + handled, iac - (buf + handled));
+			if (ret < 0)
+				return ret;
+			handled += ret;
+		} else {
+			dprintf(ios->fd, "%c%c", IAC, IAC);
+			handled += 1;
+		}
+	}
+
+	/* Send the remaining data that needs no quoting. */
+	ret = write(ios->fd, buf + handled, count - handled);
+	if (ret < 0)
+		return ret;
+	return ret + handled;
 }
 
 static ssize_t telnet_read(struct ios_ops *ios, void *buf, size_t count)

commit 679049a610f7e0e5b1b9c44afc9a62df77023860

telnet: Don't quote IAC for DONT and WONT replies
Since commit 48f4fd57802b ("telnet: Quote IAC on sending") ios_printf
quotes IAC characters. For the purpose of negotiating connection parameter
this is wrong however. So use a raw write and rename the function
accordingly.

Fixes: 48f4fd57802b ("telnet: Quote IAC on sending")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

telnet.c
@@ -29,7 +29,7 @@
 
 #include "microcom.h"
 
-static int ios_printf(struct ios_ops *ios, const char *format, ...)
+static int telnet_printf(struct ios_ops *ios, const char *format, ...)
 {
 	char buf[20];
 	int size, written = 0;
@@ -49,7 +49,7 @@ static int ios_printf(struct ios_ops *ios, const char *format, ...)
 	}
 
 	while (written < size) {
-		ret = ios->write(ios, buf + written, size - written);
+		ret = write(ios->fd, buf + written, size - written);
 		if (ret < 0)
 			return ret;
 
@@ -343,7 +343,7 @@ static int handle_command(struct ios_ops *ios, unsigned char *buf, int len)
 		} else {
 			/* unknown/unimplemented option -> DONT */
 			dbg_printf(" -> DONT\n");
-			ios_printf(ios, "%c%c%c", IAC, DONT, buf[2]);
+			telnet_printf(ios, "%c%c%c", IAC, DONT, buf[2]);
 		}
 		return 3;
 
@@ -371,7 +371,7 @@ static int handle_command(struct ios_ops *ios, unsigned char *buf, int len)
 		} else {
 			/* Oh, cannot handle that one, so send a WONT */
 			dbg_printf(" -> WONT\n");
-			ios_printf(ios, "%c%c%c", IAC, WONT, buf[2]);
+			telnet_printf(ios, "%c%c%c", IAC, WONT, buf[2]);
 		}
 		return 3;
 